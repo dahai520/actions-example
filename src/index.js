7dba957204463974b7588fb74f368006b0259973
dahai520 <87510463+dahai520@users.noreply.github.com>
2021-11-06 19:51:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export function render() {
   const div = document.createElement('div');
   div.innerHTML = `
     <div style="color:#888888">
-      <h1>Hello World 001</h1>
+      <h1>Hello World</h1>
     </div>
   `;
   document.querySelector('body').appendChild(div);
