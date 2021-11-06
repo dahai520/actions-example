e0aec03829071556fb4bb0bf0560d4f45b9e903c
dahai520 <87510463+dahai520@users.noreply.github.com>
2021-11-06 11:43:09 +0800

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,6 +2,6 @@ import { render } from '../src/index'
 
 test('test dom', async () => {
   render();
-  const expectResult: string = `<div>    <div style="color:#888888">      <h1>Hello World</h1>    </div>  </div>`;
+  const expectResult = `<div>    <div style="color:#888888">      <h1>Hello World</h1>    </div>  </div>`;
   expect(document.body.innerHTML.replace(/[\r\n]/ig, '')).toEqual(expectResult);
 });
